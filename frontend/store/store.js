a855b37ccfe13b88a7c95316dd4d3a7d19c1f0d8
Edwin Shin <edwinshin@gmail.com>
2018-02-09 09:29:30 -0800

    Make app production ready for heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
+
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
